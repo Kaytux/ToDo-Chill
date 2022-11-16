6faf14317e1a371e92c5f9cea36ac42b989ec3de
Vincent ASTOLFI <vincent.astolfi@etu.uca.fr>
2022-11-17 00:01:35 +0100

    Mise à jour de 'vues/acceuil.php'

--- vues/acceuil.php
+++ vues/acceuil.php
@@ -1,9 +1,9 @@
 <head>
 	<meta charset="utf8"/>
 	<title>Accueil</title>
+	<base href="https://codefirst.iut.uca.fr/containers/todo-chill-vincentastolfi/">
     <link rel="stylesheet" href="styles/style.css">
     <link rel="stylesheet" href="styles/homePage.css">
-	<base href="https://codefirst.iut.uca.fr/containers/todo-chill-vincentastolfi/">
 </head>
 <body>
 	<div class="btn-left-corner">
