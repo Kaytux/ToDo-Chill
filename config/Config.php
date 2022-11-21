ec6ab014433cb35a93466b09cd6cf58987ef36ec
viastolfi <vincent.astolfi@etu.uca.fr>
2022-11-21 16:44:28 +0100

    changement de base de données afin d'utiliser un container docker

--- config/Config.php
+++ config/Config.php
@@ -3,9 +3,15 @@
 $rep=__DIR__.'/../';
 
 //BD
-$dsn = "mysql:host=localhost; dbname=bdToDoChill";
-$usr = "viastolfi";
-$mdp = "MhhLeCaca1!";
+//$dsn = "mysql:host=localhost; dbname=bdToDoChill";
+//$usr = "viastolfi";
+//$mdp = "MhhLeCaca1!";
+
+$dsn='mysql:host=10.9.0.67;dbname='.getenv("MARIADB_DATABASE");
+$dbname=getenv("MARIADB_DATABASE");
+$username=getenv("MARIADB_USER");
+$passwordBD=getenv("MARIADB_PASSWORD");
+
 
 //Vues
 $vues['homePage']='vues/homePage.php';
