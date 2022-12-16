653b803fee6ec42e199c93e7560920e6fd0948b2
viastolfi <vincent.astolfi@etu.uca.fr>
2022-12-16 14:14:05 +0100

    :sparkles: Ajout de toute les fonctions d'utilisateur anonyme (V1.O de l'application)

--- controler/VisitorControler.php
+++ controler/VisitorControler.php
@@ -20,6 +20,10 @@ class VisitorControler{
                 case "connect":
                     $this->connection();
                     break;
+                case "disconnectFromUser":
+                    MdlUser::deconnection();
+                    require($rep.$vues['homePage']);
+                    break;
                 case "SignIn":
                     require($rep.$vues['signIn']);
                     break;
@@ -29,6 +33,28 @@ class VisitorControler{
                 case "continueAsAnonymous":
                     $this->continueAsAnonymous();
                     break;
+                case "addAList":
+                    $this->addAList();
+                    break;
+                case "targetAList":
+                    $this->changeTargetedList();
+                    break;
+                case "addATask":
+                    $this->addATask();
+                    break;
+                case "checkTask":
+                    $this->changeStatus("1");
+                    break;
+                case "uncheckTask":
+                    $this->changeStatus("0");
+                    break;
+                case "deleteTask":
+                    $this->deleteTask();
+                    break;
+                case "deleteList":
+                    $this->deleteList();
+                    break;
+
                 default:
                     echo "erreur page inconnue";
                     break;
@@ -66,6 +92,44 @@ class VisitorControler{
     function continueAsAnonymous(){
         global $rep, $vues;
         MdlVisitor::connection();
+        require($rep.$vues['userInterface']);
+    }
+    
+    function addAList(){
+        global $rep, $vues;
+        $name = $_POST['name'];
+        MdlUser::addAListToUser($name);
+        require($rep.$vues['userInterface']);
+    }
+
+    function changeTargetedList(){
+        global $rep, $vues;
+        $_SESSION['task'] = MdlUser::getDataTask($_POST['id']);
+        $_SESSION['targetedList'] = $_POST['id'];
+        require($rep.$vues['userInterface']);
+    }
+
+    function addATask(){
+        global $rep, $vues;
+        MdlUser::addATask($_POST['name']);
+        require($rep.$vues['userInterface']);
+    }
+
+    function changeStatus($status){
+        global $rep, $vues;
+        MdlUser::changeStatus($_POST['id'], $status);
+        require($rep.$vues['userInterface']);
+    }
+
+    function deleteTask(){
+        global $rep, $vues;
+        MdlUser::deleteTask($_POST['id']);
+        require($rep.$vues['userInterface']);
+    }
+
+    function deleteList(){
+        global $rep, $vues;
+        MdlUser::deleteList($_POST['id'], $dVueError);
         require($rep.$vues['userInterface']);
     }
 }
